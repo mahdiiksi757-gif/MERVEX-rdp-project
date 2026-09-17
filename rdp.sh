b812f648ca2ca0e4bedd3d732da84b6a15abdd92
mahdiiksi757-gif <mahdiiksi757@gmail.com>
2026-09-17 07:39:59 +0100

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -3,23 +3,18 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "==> Updating system & installing packages..."
 apt update
-apt install -y xfce4 xfce4-goodies xrdp wget curl
+apt install -y xfce4 xfce4-goodies xrdp wget curl npm
 
 echo "==> Configuring XRDP..."
 adduser xrdp ssl-cert
 /usr/sbin/xrdp-sesman
 /usr/sbin/xrdp
 
-echo "==> Setting up Ngrok..."
-# Télécharger ngrok (ila ma kanch)
-curl -sSL https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -o ngrok.tgz
-tar -xvzf ngrok.tgz
-chmod +x ngrok
+echo "==> Installing LocalTunnel..."
+npm install -g localtunnel
 
-# Hna hot token dyalk (Bddal YOUR_NGROK_AUTHTOKEN b l-authtoken dyalk mn ngrok.com)
-./ngrok config add-authtoken 3JRbECyl1QSnGFgozQtFgQJp529_2anuZbRNfWXgqAQCGdbpC
-# Demarri l-tunnel 3la port 3389
-echo "==> Starting Ngrok tunnel..."
-./ngrok tcp 3389 &
+echo "==> Starting LocalTunnel on port 3389..."
+# Kay3tina link public majani dyal locatunnel
+lt --port 3389 &
 
-echo "RDP & Ngrok Started Successfully!"
+echo "RDP & LocalTunnel Started Successfully!"
