755a4962ac62d4f33ba8f7dba7ffb463861d6492
mahdiiksi757-gif <mahdiiksi757@gmail.com>
2026-09-17 08:03:47 +0100

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -3,18 +3,12 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "==> Updating system & installing packages..."
 apt update
-apt install -y xfce4 xfce4-goodies xrdp wget curl npm
+apt install -y xfce4 xfce4-goodies xrdp ssh
 
 echo "==> Configuring XRDP..."
 adduser xrdp ssl-cert
 /usr/sbin/xrdp-sesman
 /usr/sbin/xrdp
 
-echo "==> Installing LocalTunnel..."
-npm install -g localtunnel
-
-echo "==> Starting LocalTunnel on port 3389..."
-# Kay3tina link public majani dyal locatunnel
-lt --port 3389 &
-
-echo "RDP & LocalTunnel Started Successfully!"
+echo "==> Starting Pinggy TCP Tunnel..."
+ssh -p 443 -R0:localhost:3389 qr@a.pinggy.io -o StrictHostKeyChecking=no
