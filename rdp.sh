41562f2d6d8311d2bd9f6bb7c1d73b6509384bfc
mahdiiksi757-gif <mahdiiksi757@gmail.com>
2026-09-17 07:28:56 +0100

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -3,5 +3,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install -y xfce4 xfce4-goodies xrdp
 adduser xrdp ssl-cert
-systemctl restart xrdp
-echo "RDP Ready!"
+/usr/sbin/xrdp-sesman
+/usr/sbin/xrdp
+echo "RDP Started Successfully!"
